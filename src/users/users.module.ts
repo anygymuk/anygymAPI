9e1866b045bba40572503d594146616cac6ac75a
Naaman hudson <naamanh10@gmail.com>
2025-11-26 10:48:37 +0000

    /user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { Auth0Guard } from './guards/auth0.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, Auth0Guard],
 })
 export class UsersModule {}
 
