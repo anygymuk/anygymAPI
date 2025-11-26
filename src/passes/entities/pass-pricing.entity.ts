894ffa2d1efec0400f80042526dc065db6225cb1
Naaman hudson <naamanh10@gmail.com>
2025-11-26 13:36:19 +0000

    deploy fix

--- src/passes/entities/pass-pricing.entity.ts
+++ src/passes/entities/pass-pricing.entity.ts
@@ -2,7 +2,7 @@ import { Entity, Column, PrimaryColumn } from 'typeorm';
 
 @Entity('pass_pricing')
 export class PassPricing {
-  @PrimaryColumn()
+  @PrimaryColumn({ name: 'subscription_tier' })
   tier: string;
 
   @Column({ name: 'default_price', type: 'decimal', precision: 10, scale: 2 })
